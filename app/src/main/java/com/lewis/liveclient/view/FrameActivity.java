452092b8927a856ad22d1eb77cb8a83968211525
luoyu <303132289@qq.com>
2017-12-10 22:29:09 +0800

    fix README

--- app/src/main/java/com/lewis/liveclient/view/FrameActivity.java
+++ app/src/main/java/com/lewis/liveclient/view/FrameActivity.java
@@ -1,5 +1,10 @@
 package com.lewis.liveclient.view;
 
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+
+import com.lewis.liveclient.Annotation.Bind;
 import com.lewis.liveclient.R;
 import com.lewis.liveclient.animation.MyCountView;
 import com.lewis.liveclient.view.BaseActivity;
@@ -10,9 +15,23 @@ import com.lewis.liveclient.view.BaseActivity;
  */
 
 public class FrameActivity extends BaseActivity {
+    @Bind(R.id.start_live)
+    private Button startLiveButton;
 
     @Override
     protected void init() {
         setContentView(MyCountView.initView(this, R.layout.activity_main));
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        startLiveButton.setText("开始直播");
+    }
+
+    public void onClick(View view) {
+        Intent intent = new Intent();
+        intent.setClass(this, LiveActivity.class);
+        startActivityForResult(intent, 1);
     }
 }
